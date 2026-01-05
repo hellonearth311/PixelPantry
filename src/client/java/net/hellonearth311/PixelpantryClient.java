c3ac214bfa9b8f131088b850d4e5ca58289103f9
swaritnarang <naranswarit02@gmail.com>
2026-01-05 15:42:10 -0600

    add json files for tomato crop, currently i'm using a cauliflower texture that i already have.

--- src/client/java/net/hellonearth311/PixelpantryClient.java
+++ src/client/java/net/hellonearth311/PixelpantryClient.java
@@ -1,10 +1,13 @@
 package net.hellonearth311;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.hellonearth311.registries.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class PixelpantryClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TOMATO_CROP, RenderLayer.getCutout());
 	}
 }
